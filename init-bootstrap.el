0820868951578d4845906ac7d180eaf2c355b828
Saulo S. Toledo <saulotoledo@gmail.com>
2025-11-08 18:47:07 +0100

    fix: correct variable scoping and address lisp linting issues
    Ensures that configuration variables intended to be applied globally are correctly initialized using `setq-default` instead of `setq`. This fixes cases where settings were incorrectly applied buffer-locally.
    
    Also includes minor cosmetic and style fixes across the configuration to satisfy linter rules.

--- init-bootstrap.el
+++ init-bootstrap.el
@@ -1,9 +1,13 @@
-;;; -*- lexical-binding: t; -*-
+;;; init-bootstrap.el --- Personal bootstraping file -*- lexical-binding: t; -*-
+
+;;; Commentary:
 
 ;; Rename this file to init.el. It will replace itself with the
 ;; actual configuration at first run.
 ;; We could use `git update-index --assume-unchanged init.el`, but
 ;; I find it easier to rename it.
+
+;;; Code:
 
 (require 'org)
 (find-file (concat user-emacs-directory "init.org"))
@@ -14,5 +18,6 @@
 ;; generates native compilation files (.eln) instead of .elc:
 ;; (byte-compile-file (concat user-emacs-directory "init.el"))
 ;; We use native compilation instead:
-(setq native-comp-deferred-compilation t)
+(setq native-comp-jit-compilation t)
 (native-compile-async (concat user-emacs-directory "init.el"))
+;;; init-bootstrap.el ends here
